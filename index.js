ecede70bf955d60a41c4a5b01513b0dc9d2ab469
Job Muturi <muirimuturi17@gmail.com>
2023-03-29 15:08:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; 1 <word.length/2; i++){
+    let j = word.length-1-i;
+    if(word[i] !== word[j]) return false;
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate over all the words from the begining to the middle and from the back to the middle.
+  if the words from the begining and the words from behind match
+  return true if not return false
 */
 
 /*
